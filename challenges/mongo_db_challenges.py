1d9e6b0fe6bd52f70073676b2ae2f3ba86df90f6
Derek Gossi <gossiderek@gmail.com>
2015-02-25 14:59:19 -0500

    challenges 1 to 3

---
+++ challenges/mongo_db_challenges.py
@@ -0,0 +1,67 @@
+import pickle
+
+# Open heavy metal reviews dataframe 
+with open("heavy_metal_parsed.pkl", 'r') as datafile:
+    hm_reviews = pickle.load(datafile)
+
+# Initialize MongoDB client
+from pymongo import MongoClient
+
+client = MongoClient()
+
+# Insert reviews into DB
+hmm = client.dsbc.hmm
+for review in hm_reviews:
+    hmm.save(review)
+
+
+### CHALLENGE #1
+
+import matplotlib.pyplot as plt
+import seaborn as sb
+%matplotlib inline
+
+# Get cursor of years
+hmm_yr_c = hmm.find( {} , { 'year' } )
+
+hmm_yrs = [ ele['year'] for ele in hmm_yr_c ]
+
+# Plot histogram of years
+plt.hist(hmm_yrs)
+plt.show()
+
+
+### CHALLENGE #2
+
+from collections import Counter
+
+# Get cursor of cast members
+hmm_cast_c = hmm.find( {} , { 'cast' } )
+
+# Count cast members
+cast_cnt = Counter()
+for ele in hmm_cast_c:
+    for cast_member in ele['cast']:
+        cast_cnt[cast_member] += 1
+   
+# Sort and print 
+srt_cast_cnt = sorted( cast_cnt.items(), key=operator.itemgetter(1), reverse=True )
+print srt_cast_cnt[1:10]
+
+
+### CHALLENGE #3
+
+# Get cursor of title
+hmm_title_c = hmm.find( {} , { 'title' } )
+
+title_wrd_cnt = Counter()
+
+# Count title words
+for ele in hmm_title_c:
+    for word in ele['title'].split():
+        title_wrd_cnt[word] += 1
+
+# Sort and print 
+srt_title_wrd_cnt = sorted( title_wrd_cnt.items(), key=operator.itemgetter(1), reverse=True )
+print srt_title_wrd_cnt[1:10]
+
